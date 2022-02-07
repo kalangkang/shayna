6bf50bd18ea13822cfc456b07df80a23a792ad9f
dodisyaripudin <dodisyaripudin@gmail.com>
2022-02-07 23:13:32 +0700

    Upload
    First Upload

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna/>
+    <HeroShayna/>
+    <WomenShayna/>
+    <GalleryShayna/>
+    <PartnerShayna/>
+    <FooterShayna/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomenShayna from '@/components/WomenShayna.vue'
+import GalleryShayna from '@/components/GalleryShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    GalleryShayna,
+    PartnerShayna,
+    FooterShayna,
+  }
+}
+</script>
